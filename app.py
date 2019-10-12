c04c8bc172056a65ce352ed458d7bceeb4bf2359
jackhwolf <jwolf22@wisc.edu>
2019-10-12 15:41:12 -0500

    cors?

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from backend.userddbconn import userddbconn
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"*": {"origins": "http://localhost:port"}})
+CORS(app)
 
 app.config['SECRET_KEY'] = 'liuytvryuybindsty5ur6t7yu'
 app.config['CORS_HEADERS'] = 'Content-Type'
