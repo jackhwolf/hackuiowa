e0119b06709a74627f03bb66dd937a6262f9e4a9
jackhwolf <jwolf22@wisc.edu>
2019-10-12 15:05:11 -0500

    fixs

--- blueprints/api.py
+++ blueprints/api.py
@@ -26,8 +26,7 @@ def getapibp():
             parser.add_argument('username', required=True)
             parser.add_argument('password', required=True)
             args = parser.parse_args()
-            resp = udb.doesUserExist(**args)
-            return {'Response': udb.doesUserExist(**args)}
+            return udb.doesUserExist(**args)
 
         def post(self):
             ''' add/delete new user '''
