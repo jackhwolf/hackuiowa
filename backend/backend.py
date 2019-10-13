01115ab2373e75761f43a0ce7178f2423c9b9e34
jackhwolf <jwolf22@wisc.edu>
2019-10-12 20:21:51 -0500

    rainfall

--- backend/backend.py
+++ backend/backend.py
@@ -10,5 +10,5 @@ def getrainfall(lat=37.8267, long=-122.4233):
     url = f"https://api.darksky.net/forecast/{os.environ.get('weatherKey')}/{str(lat)},{str(long)}"
     r = requests.get(url).json()
     daily = r['daily']['data']
-    print(list(map(lambda x: [x['time'], x['precipProbability'], x['precipIntensityMax']], daily)))
-    return r
+    daily = list(map(lambda x: [x['time'], x['precipProbability'], x['precipIntensityMax']], daily))
+    return daily
