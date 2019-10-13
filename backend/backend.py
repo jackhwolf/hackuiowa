e3e509e067e4f6d6021f8d91e53315d6dcd4379c
jackhwolf <jwolf22@wisc.edu>
2019-10-12 20:23:37 -0500

    rainfall

--- backend/backend.py
+++ backend/backend.py
@@ -2,13 +2,15 @@ import requests
 from dotenv import load_dotenv
 import os
 import json
-
+import time
 
 load_dotenv()
 
 def getrainfall(lat=37.8267, long=-122.4233):
     url = f"https://api.darksky.net/forecast/{os.environ.get('weatherKey')}/{str(lat)},{str(long)}"
-    r = requests.get(url).json()
+    r = requests.get(url)
+    time.sleep(2)
+    r = r.json()
     daily = r['daily']['data']
     daily = list(map(lambda x: [x['time'], x['precipProbability'], x['precipIntensityMax']], daily))
     return daily
