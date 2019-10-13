f72a3d7c7bc6bf9d32f842f52b22006eb1ccfe3d
jackhwolf <jwolf22@wisc.edu>
2019-10-13 06:35:34 -0500

    work

--- backend/logic.py
+++ backend/logic.py
@@ -44,14 +44,14 @@ class logic:
         return r[0]['elevation']
 
     # compare your elev to elev of surrounding areas
-    def relativedanger(self, addr, r=0.03, n=1):
+    def relativedanger(self, addr, r=0.03, n=2):
         center = self.addrlatlng(addr)
         master = self.getelev(center[0], center[1])
         hits = 0
         for x in range(0, n+1):
             lat, lng = (
-                center[0]+(math.cos(2 * math.pi / n * x) * r),   # x
-                center[1] + (math.sin(2 * math.pi / n * x) * r)  # y
+                center[0] + (math.cos(2 * math.pi / n * x) * r),  # x
+                center[1] + (math.sin(2 * math.pi / n * x) * r)   # y
             )
             elev = self.getelev(lat, lng)
             if master < elev:
@@ -59,17 +59,18 @@ class logic:
         return hits/n
 
     # provide human readable summary of results
-    def summarize(rainfall, danger):
-        m = max(list(rainfall), key=lambda x: rainfall[x])
+    def summarize(self, rainfall, danger):
+        m = 3
         s = f"You live in a lower elevation than {danger*100}% of people in the area, "
-        _ += "which puts you at a comparatively {} risk of flooding."
-        if s > 0.5:
+        _ = "which puts you at a comparatively {} risk of flooding."
+        if danger > 0.5:
             s += _.format("higher")
         else:
             s += _.format("lower")
-        if m < 24:
-            s += "However, there will only be a total of {m} inches "
+        if m < 12:
+            s += f"However, there will only be a total of {m} inches "
             s += "of rain this week, so there is no flood risk"
         else:
-            s += "This week there will be a total of {m} inches "
-            s+= "of rain, so watch out"
+            s += f"This week there will be a total of {m} inches "
+            s += "of rain, so watch out"
+        return s
