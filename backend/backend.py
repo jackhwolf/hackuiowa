d22814d469e03aac1da2b94ab757b2232e315030
jackhwolf <jwolf22@wisc.edu>
2019-10-12 19:31:34 -0500

    rainfall

--- backend/backend.py
+++ backend/backend.py
@@ -7,4 +7,7 @@ load_dotenv()
 def rainfall(lat=37.8267, long=-122.4233):
     url = f"https://api.darksky.net/forecast/{os.environ.get('weatherKey')}/{str(lat)},{str(long)}"
     r = requests.get(url).json()
+    print(os.environ.get('weatherKey'))
+    w = os.environ.get('weatherKey')
+    print(w)
     return r
