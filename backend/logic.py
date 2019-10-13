bdaf21765f507fd28f7e335c74d0027b6c93c754
jackhwolf <jwolf22@wisc.edu>
2019-10-13 03:25:20 -0500

    msg

--- backend/logic.py
+++ backend/logic.py
@@ -21,6 +21,9 @@ class logic:
         addr = addr.replace(' ', '%20')  # delim --> `space`
         maboxKey = os.environ.get('mapboxKey')
         url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{addr}.json?access_token={os.environ.get('mapboxKey')}"
+        print()
+        print(r)
+        print()
         r = requests.get(url).json()['features'][0]['center']
         return r[::-1]
 
